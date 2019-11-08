2e63402f5af19d8fa195fcc21e6c1afc0d05af78
Ledka17 <ledkakonf@gmail.com>
2019-11-08 11:58:11 +0300

    fix swGenerator

--- src/scripts/generateSWUrlsToCache.js
+++ src/scripts/generateSWUrlsToCache.js
@@ -3,13 +3,16 @@ const fs = require("fs");
 
 const EXCLUDED_DATA = [".DS_Store", "index.html", "urlsToCache.json"];
 
+const addAbsolutePath = fileName => "./" + fileName;
+
 async function generate() {
     try {
-        const files_public = await recursiveReaddir("../public", EXCLUDED_DATA);
-        const files_src = await recursiveReaddir("../src", EXCLUDED_DATA);
+        const files_public = await recursiveReaddir("./public", EXCLUDED_DATA);
+        const files_src = await recursiveReaddir("./src", EXCLUDED_DATA);
+        const files = files_public.concat(files_src);
         fs.writeFileSync(
-            "./urlsToCache.json",
-            JSON.stringify({ cache: files_public.concat(files_src) })
+            "./src/urlsToCache.json",
+            JSON.stringify({ cache: files.map(addAbsolutePath) })
         );
     } catch (error) {
         console.error(error);
