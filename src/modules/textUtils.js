abe481b35e26c570d6a4a6a31e51f82b34325835
anton <anton.cobzew@yandex.ru>
2019-11-08 12:06:31 +0300

    Fix

--- src/modules/textUtils.js
+++ src/modules/textUtils.js
@@ -1,10 +1,7 @@
 const ELLIPSIS = "...";
 
-class TextUtils {
-    cutIfLong(text, limit) {
+export default class TextUtils {
+    static cutIfLong(text, limit) {
         return text.slice(0, limit) + (text.length > limit ? ELLIPSIS : "");
     }
 }
-
-const textUtils = new TextUtils();
-export default textUtils;
