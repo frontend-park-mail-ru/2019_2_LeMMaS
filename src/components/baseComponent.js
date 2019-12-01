c08d80f553713c0709e125f5537c9b6c2182aa53
anton <anton.cobzew@yandex.ru>
2019-12-01 12:03:29 +0300

    Mobile fixes

--- src/components/baseComponent.js
+++
@@ -1,7 +0,0 @@
-export default class BaseComponent {
-    constructor(parent) {
-        this.parent = parent;
-    }
-
-    render() {}
-}
---
+++ src/components/header/index.ts
@@ -0,0 +1,27 @@
+import { html } from "common-tags";
+import classNames from "classnames";
+
+import BaseComponent from "components/baseComponent";
+import Logo from "components/logo";
+import Menu from "components/menu";
+
+import "./style.css";
+
+export default class Header extends BaseComponent {
+    public render() {
+        const headerClass = classNames("header", {
+            "header_show-home-button": this._isShowHomeButton(),
+        });
+        this.parent.innerHTML = html`
+            <header class="${headerClass}">
+                ${new Logo().renderString()}
+                <div class="menu"></div>
+            </header>
+        `;
+        new Menu(this.parent.querySelector(".menu")).start();
+    }
+
+    private _isShowHomeButton(): boolean {
+        return location.pathname !== "/";
+    }
+}
